c2ccde842316e40ebcee8a5dc378f45df4c7e451
Maria <lourdes1984.sosa@gmail.com>
2023-11-04 00:34:37 -0300

    [cfg] - base structured

---
+++ README.md
@@ -0,0 +1,4 @@
+# PROYECTO FINAL DE CODO A CODO
+
+[Testing Library](https://www.robinwieruch.de/vitest-react-testing-library/)
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola Mundo por enesima vez</h1>
+    </div>
+  );
+};
+
+export default App;

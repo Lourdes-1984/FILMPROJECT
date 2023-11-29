ef34398a098599b80789821b2c50ed653b7a6c70
Maria <lourdes1984.sosa@gmail.com>
2023-11-29 15:00:44 -0300

    [cfg]- pruebas de deployer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hola Mundo </h1>
+      <h1>APP PARA PELIS </h1>
       <h2>Prueba de deployer</h2>
     </div>
   );

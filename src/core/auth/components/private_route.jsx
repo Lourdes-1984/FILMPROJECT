01ea4822a54898fda91607a3ff9938058aa8368e
Maria <lourdes1984.sosa@gmail.com>
2023-11-30 01:10:30 -0300

    [cfg]-localstorage

--- src/core/auth/components/private_route.jsx
+++ src/core/auth/components/private_route.jsx
@@ -2,11 +2,11 @@ import React, { Children } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hook/use_auth";
 
-const PirvateRoute = ({ children }) => {
+const PrivateRoute = ({ children }) => {
   const { isLoggedIn } = useAuth();
 
   if (isLoggedIn) return children;
   return <Navigate to={"/login"} />;
 };
 
-export default PirvateRoute;
+export default PrivateRoute;
--- src/core/routes/app_router.jsx
+++ src/core/routes/app_router.jsx
@@ -1,19 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomeView from "../../features/home/views/HomeView";
 import LoginView from "../../features/login/views/LoginView";
-import PirvateRoute from "../auth/components/private_route";
+import PrivateRoute from "../auth/components/private_route";
+import PublicRoute from "../auth/components/public_route";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
     element: (
-      <PirvateRoute>
+      <PrivateRoute>
         <HomeView />
-      </PirvateRoute>
+      </PrivateRoute>
     ),
   },
   {
     path: "/login",
-    element: <LoginView />,
+    element: (
+      <PublicRoute>
+        <LoginView />
+      </PublicRoute>
+    ),
+  },
+  //Manejar error 404
+  {
+    path: "*",
+    element: <div>No se encontro la página</div>,
   },
 ]);
